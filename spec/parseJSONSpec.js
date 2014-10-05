7477078c998e90623141a508a4f0aeae3786ea0b
thejsj <jorge.silva@thejsj.com>
2014-10-05 13:59:49 -0400

    Finishig parseJSON and cleaning up

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -1,21 +1,11 @@
 // test cases are described in fixtures.js
 describe('parseJSON', function () {
-  var counter = 0;
+
   it('should match the result of calling JSON.parse', function () {
     parseableStrings.forEach(function (test) {
-      counter += 1;
-      console.log('------   ' + counter + '  -------');
-      console.log('test');
-      console.log(test);
-      console.log('expected');
-      var expected = JSON.parse(test);
-      console.log(expected);
-      console.log('result');
       var result = parseJSON(test);
-      console.log(result);
-      console.log('equality');
+      var expected = JSON.parse(test);
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
-      console.log(equality);
       expect(equality).to.equal(true);
     });
   });
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,7 +3,7 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function (json) {
-  // your code goes here
+  // High order function to be used for detecting type
   var firstAndLastChars = function (first, last) {
     return function (str) {
       return str[0] === first && str[str.length - 1] === last;
@@ -15,7 +15,7 @@ var parseJSON = function (json) {
   var hasSingleQuotes = firstAndLastChars("'", "'");
   var isString = function (str) {
     str = removeSpaces(str);
-    return hasSingleQuotes(str) || hasDoubleQuotes(str);
+    return (hasSingleQuotes(str) || hasDoubleQuotes(str)) && str[str.length - 2] !== '\\';
   };
   var isNumber = function (str) {
     return (+(str)) + '' === str;
@@ -27,80 +27,96 @@ var parseJSON = function (json) {
     str = removeSpaces(str);
     return str.substring(1).slice(0, str.length - 2) || '';
   };
-  var parseStringIntoKeyValuePairs = function (str, result) {
-    if (str === '') return result;
-    var string_open = false;
-    var array_open = false;
-    var object_open = false;
-    var array_bracket_count = 0;
-    var object_bracket_count = 0;
-    var current = '';
-    for (var i = 0; i < str.length; i += 1) {
-      var ch = str[i];
-      if (ch === '"' || ch === "'") {
-        string_open = !string_open;
+  // High order function to be used for splitting string
+  var splitByChar = function (base_char) {
+    return function (str) {
+      var result = [];
+      var double_string_open = false;
+      var single_string_open = false;
+      var array_open = false;
+      var object_open = false;
+      var array_bracket_count = 0;
+      var object_bracket_count = 0;
+      var curr_str = '';
+      var prev_ch = '';
+      for (var i = 0; i < str.length; i += 1) {
+        var ch = str[i];
+        if (ch === '"') {
+          double_string_open = !double_string_open;
+        }
+        if (ch === "'") {
+          single_string_open = !single_string_open;
+        }
+        if (ch === '[') {
+          array_bracket_count += 1;
+          array_open = true;
+        }
+        if (ch === ']') {
+          array_bracket_count -= 1;
+          if (array_bracket_count === 0) {
+            array_open = false;
+          }
+        }
+        if (ch === '{') {
+          object_bracket_count += 1;
+          object_open = true;
+        }
+        if (ch === '}') {
+          object_bracket_count -= 1;
+          if (object_bracket_count === 0) {
+            object_open = false;
+          }
+        }
+        if (ch === base_char && !double_string_open && !single_string_open && !array_open && !object_open) {
+          if (curr_str !== '') result.push(removeSpaces(curr_str));
+          curr_str = '';
+          prev_ch = '';
+        } else {
+          curr_str += ch;
+          prev_ch = ch;
+        }
       }
-    }
-    console.log(str);
-    return result;
+      if (curr_str !== '') result.push(removeSpaces(curr_str));
+      return result;
+    };
   };
-  var parseJSONString = function (str) {
+  var separeateStringByCommas = splitByChar(',');
+  var separeateStringByColons = splitByChar(':');
+  var parseJSONString = function (str, parent) {
     str = removeSpaces(str);
     if (isArray(str)) {
-      var array = removeFirstAndLastChar(str).split(',');
+      var array = separeateStringByCommas(removeFirstAndLastChar(str));
       if (array.length === 1 && array[0] === '') return [];
       return array.map(parseJSONString);
     } else if (isObj(str)) {
-      var _obj = {};
       /**
        * I wanted to do something elegant with regular expressions that could
        * parse object key value pairs nicely with regular expressions, but
        * Douglas Crockford seems to think that's a bad idea!
        * https://github.com/douglascrockford/JSON-js/blob/master/json_parse.js
        */
-      window.regex = /(("[^"]+"[^,]*):[ ]{0,1}([A-z0-9]+|(\[[^\]]+\]*))|("[^"]+"[^,]*))(?:[,]{0,1}?)/g;
-      window.str = removeFirstAndLastChar(str);
-      var tab = '     ';
-      var _str = removeFirstAndLastChar(str);
-      var _split = _str.split(',');
-      var _regex = parseStringIntoKeyValuePairs(_str, []);
-      var obj = _regex || _split;
-      console.log(tab + 'Split!');
-      console.log(tab + _str);
-      console.log(tab + 'split(,)');
-      console.log(tab + _split);
-      console.log(tab + 'Reg Exp: ' + regex.toString());
-      console.log(tab + _regex);
-      console.log(tab + 'OJB:');
-      console.log(obj);
-      console.log(tab + 'EQUAL ? ' + (_split == _regex));
-      // obj is an array of strings with key: value
-      obj.forEach(function (val, i) {
-        window.regex_split = /({[^}]+}|[^:]+)/g;
-        var split = val.match(regex_split) || val.split(':');
-        console.log('SPLIT ":"');
-        console.log(split);
-        console.log(split.length);
-        if (split.length === 2) {
-          _obj[parseJSONString(split[0])] = parseJSONString(split[1]);
+      var obj = {};
+      var _obj = separeateStringByCommas(removeFirstAndLastChar(str));
+      // _obj is an array of strings with 'key: value'
+      _obj.forEach(function (val, i) {
+        var key_val_pair = separeateStringByColons(val); // split into key, value
+        if (key_val_pair.length === 2) {
+          obj[parseJSONString(key_val_pair[0])] = parseJSONString(key_val_pair[1]);
         }
       });
-      console.log('_obj');
-      console.log(_obj);
-      return _obj;
+      return obj;
     } else if (isString(str)) {
-      return removeFirstAndLastChar(str);
+      // unescape string
+      return removeFirstAndLastChar(str).replace(/([\\]{1})([\\\"]{1})/g, '$2');
     } else
     if (isNumber(str)) {
       return +str;
-    } else {
-      if (str === 'null') return null;
-      if (str === 'false') return false;
-      if (str === 'true') return true;
-      if (str === 'undefined') return undefined;
-      console.log('NOT FOUND');
-      throw new Error('String could not be parsed');
     }
+    if (str === 'null') return null;
+    if (str === 'false') return false;
+    if (str === 'true') return true;
+    if (str === 'undefined') return undefined;
+    throw new SyntaxError('Unexpected end of input');
   };
   return parseJSONString(json);
 };
