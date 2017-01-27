37dc3ae0d4f9cee9ffffdf80a845813d4b6de911
thejsj <jorge.silva@thejsj.com>
2017-01-26 20:03:43 -0800

    Refactor

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,48 +1,20 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
-
-// From underscore.js (modified)
-// https://github.com/jashkenas/underscore/blob/master/underscore.js#L116-L129
-// Return the results of applying the iterate to each element.
-var map = _.collect = function (obj, iteratee, context) {
-  if (obj == null) return [];
-  var keys = obj.length !== +obj.length && Object.keys(obj),
-    length = (keys || obj).length,
-    results = Array(length),
-    currentKey;
-  for (var index = 0; index < length; index++) {
-    currentKey = keys ? keys[index] : index;
-    results[index] = iteratee(obj[currentKey], currentKey, obj);
-  }
-  return results;
-};
-
-// but you don't so you're going to write it from scratch:
-var stringifyJSON = function (obj) {
-  // your code goes here
-  var appendVariableString = function (obj) {
-    if (Array.isArray(obj)) {
-      return '[' + obj.map(appendVariableString).join(',') + ']';
-    } else if (obj === null) {
-      return 'null';
-    } else if (typeof obj === 'object') {
-      var _obj = map(obj, function (val, key) {
-        if (appendVariableString(val)) {
-          return appendVariableString(key) + ":" + appendVariableString(val);
-        }
-      }).filter(function (val) {
-        return val !== undefined;
-      });
-      return '{' + _obj.join(',') + '}';
-    } else if (typeof obj === 'number' || typeof obj === 'boolean') {
-      return obj.toString();
-    } else if (typeof obj === 'string') {
-      return '"' + obj.toString() + '"';
-    } else if (typeof obj !== 'function' && obj !== undefined) {
-      throw new Error(typeof obj + 'TYPE NOT SUPPORTED');
-    } else {
-      return false;
+const stringifyJSON = function (obj) {
+  const appendVariableString = (obj) => {
+    if (Array.isArray(obj)) return '[' + obj.map(appendVariableString).join(',') + ']'
+    if (obj === null) return 'null'
+    if (typeof obj === 'number' || typeof obj === 'boolean') return obj.toString()
+    if (typeof obj === 'string') return '"' + obj.toString() + '"'
+    if (typeof obj === 'object') {
+      var _obj = Object.key(obj)
+        .map((key) => {
+          const val = obj[key]
+          if (appendVariableString(val)) return appendVariableString(key) + ":" + appendVariableString(val)
+        })
+        .filter(val => val !== undefined)
+      return '{' + _obj.join(',') + '}'
     }
-  };
-  return appendVariableString(obj);
+    if (typeof obj !== 'function' && obj !== undefined) throw new Error(typeof obj + 'TYPE NOT SUPPORTED')
+    return false
+  }
+  return appendVariableString(obj)
 };
